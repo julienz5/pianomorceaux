e888950fd8a40228dd22f74e6bf3691a235345f1
julienz5 <julien.zana5@gmail.com>
2015-11-12 18:09:03 +0100

    pianomorceaux

---
+++ global.js
@@ -0,0 +1,38 @@
+// Custom JS
+$(document).ready(function(){
+	
+  // Add your code here
+
+  //.........................................................................Smooth Scroll
+   	$(document).ready(function() {
+ 	$('.scrollTo').click( function() { // Au clic sur un élément
+ 	var page = $(this).attr('href'); // Page cible
+ 	var speed = 750; // Durée de l'animation (en ms)
+ 	$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+ 	return false;
+ 	});
+ 	});
+
+ //.........................................................................Pop Over
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover(); 
+	});
+
+ //.........................................................................Dico Accords Gad
+
+	$(document).ready(function() {
+		$("#go").click(function(event) {
+			//console.log($( "#accord").val(),$( "#minmaj").val(),$( "#dim").val());
+			var src = $("#accord").val() + $("#minmaj").val() + $("#dim").val() + ".jpg";
+			console.log(src);
+			//$("#output").attr('src', src.toLowerCase()).fadeIn(1500);
+			$("#output").fadeOut(500, function() {
+	        	$("#output").attr("src", "img/accord/" + src.toLowerCase());
+	    	}).fadeIn(1800);
+		});
+	});
+
+
+
+
+});
