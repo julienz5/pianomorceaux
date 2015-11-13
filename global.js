65352401648daabcbd0a505ecb2f2fb052d3fa53
julienz5 <julien.zana5@gmail.com>
2015-11-13 15:12:36 +0100

    a

--- global.js
+++ global.js
@@ -1,26 +1,25 @@
 // Custom JS
 $(document).ready(function(){
-	
+	$(".more_text").hide();
   // Add your code here
 
   //.........................................................................Smooth Scroll
-   	$(document).ready(function() {
+
  	$('.scrollTo').click( function() { // Au clic sur un élément
- 	var page = $(this).attr('href'); // Page cible
- 	var speed = 750; // Durée de l'animation (en ms)
- 	$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
- 	return false;
- 	});
+	 	var page = $(this).attr('href'); // Page cible
+	 	var speed = 750; // Durée de l'animation (en ms)
+	 	$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+	 	return false;
  	});
 
+
  //.........................................................................Pop Over
-$(document).ready(function(){
+
     $('[data-toggle="popover"]').popover(); 
-	});
+
 
  //.........................................................................Dico Accords Gad
 
-	$(document).ready(function() {
 		$("#go").click(function(event) {
 			//console.log($( "#accord").val(),$( "#minmaj").val(),$( "#dim").val());
 			var src = $("#accord").val() + $("#minmaj").val() + $("#dim").val() + ".jpg";
@@ -30,9 +29,25 @@ $(document).ready(function(){
 	        	$("#output").attr("src", "img/accord/" + src.toLowerCase());
 	    	}).fadeIn(1800);
 		});
-	});
 
+ //.........................................................................More text		
 
+$('a.read_more').click(function(event){ /* find all a.read_more elements and bind the following code to them */
+        event.preventDefault(); /* prevent the a from changing the url */
+        $(this).hide();
+        $(this).parents('.item').find('.more_text').fadeIn(1000); /* show the .more_text span */
+        //GA
+        $(this).parents('.item').find('.more_text').append("<a href='#'class='read_less'>Read less</a>");
+        $( ".read_less" ).bind( "click", function() {
+        	event.preventDefault();
+  		$(this).hide();
+        $(this).parents('.item').find('.more_text').hide(); /* show the .more_text span */
+        $(this).parents('.item').find('.read_more').fadeIn(
+		'fast');
+		});
+
+		
+    });
 
 
 });
